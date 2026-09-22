15a9f71af580c4a53ba3961e89e07dfdc8a6009b
Zalven Dayao <zalvendayao888@gmail.com>
2026-09-22 17:15:18 +0800

    feat: adding agent skills

--- pkg/service/service.domain.go
+++ pkg/service/service.domain.go
@@ -1,1 +1,20 @@
 package service
+
+type APIService interface{}
+type AuthService interface{}
+type BroadcastService interface{}
+type CacheService interface{}
+type ConfigService interface{}
+type SQLService interface{}
+type NOSQLService interface{}
+type CQRSService interface{}
+type LoggingService interface{}
+type OTPService interface{}
+type QRService interface{}
+type ReportService interface{}
+type CronService interface{}
+type SecurityService interface{}
+type SMSService interface{}
+type SMTPService interface{}
+type StorageService interface{}
+type StreamService interface{}
