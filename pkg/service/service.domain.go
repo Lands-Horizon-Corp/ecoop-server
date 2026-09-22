35ab755bd0aa3a054da9d667753571acc9add0ed
Zalven Dayao <zalvendayao888@gmail.com>
2026-09-22 17:17:41 +0800

    feat: adding agent skills

--- pkg/service/service.domain.go
+++ pkg/service/service.domain.go
@@ -1,20 +1,76 @@
 package service
 
-type APIService interface{}
-type AuthService interface{}
-type BroadcastService interface{}
-type CacheService interface{}
-type ConfigService interface{}
-type SQLService interface{}
-type NOSQLService interface{}
-type CQRSService interface{}
-type LoggingService interface{}
-type OTPService interface{}
-type QRService interface{}
-type ReportService interface{}
-type CronService interface{}
-type SecurityService interface{}
-type SMSService interface{}
-type SMTPService interface{}
-type StorageService interface{}
-type StreamService interface{}
+import "context"
+
+type APIService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type AuthService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type BroadcastService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type CacheService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type ConfigService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type SQLService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type NOSQLService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type CQRSService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type LoggingService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type OTPService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type QRService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type ReportService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type CronService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type SecurityService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type SMSService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type SMTPService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type StorageService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
+type StreamService interface {
+	Init(ctx context.Context) error
+	Shutdown(ctx context.Context) error
+}
